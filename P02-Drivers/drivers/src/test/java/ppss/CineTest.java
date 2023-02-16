14e8ea976bfb49c5731594aa7faa69a603a3da70
Javier Mellado Sanchez <jms138@alu.ua.es>
2023-02-17 00:58:54 +0100

    Ejercicio P02-4 terminado

--- P02-Drivers/drivers/src/test/java/ppss/CineTest.java
+++ P02-Drivers/drivers/src/test/java/ppss/CineTest.java
@@ -1,6 +1,12 @@
 package ppss;
 
+import java.util.stream.Stream;
+
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CineTest {
@@ -71,6 +77,28 @@ class CineTest {
         assertAll(
                 ()->assertEquals(resultadoEsperado,resultado),
                 ()->assertArrayEquals(resultadoEsperadoAsientos,asientos)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("testParametrizado")
+    @Tag("parametrizado")
+    void reservaButacasC5(boolean asientos[], int solicitado, boolean resultadoEsperado, boolean resultadoEsperadoAsientos[]){
+        Cine cine = new Cine();
+        boolean resultado = cine.reservaButacasV2(asientos,solicitado);
+
+        assertAll(
+                ()->assertEquals(resultadoEsperado,resultado),
+                ()->assertArrayEquals(resultadoEsperadoAsientos,asientos)
+        );
+    }
+
+    private static Stream<Arguments> testParametrizado(){
+        return Stream.of(
+                Arguments.of(new boolean[0], 3, false, new boolean[0]),
+                Arguments.of(new boolean[0], 0, false, new boolean[0]),
+                Arguments.of(new boolean[]{false, false, false, true, true}, 2, true, new boolean[]{true,true,false,true,true}),
+                Arguments.of(new boolean[]{true,true,true}, 1, false, new boolean[]{true,true,true})
         );
     }
 }
--- P02-Drivers/drivers/src/test/java/ppss/DataArrayTest.java
+++ P02-Drivers/drivers/src/test/java/ppss/DataArrayTest.java
@@ -1,6 +1,12 @@
 package ppss;
 
+import java.util.stream.Stream;
+
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import static org.junit.jupiter.api.Assertions.*;
 
 class DataArrayTest {
@@ -134,5 +140,53 @@ class DataArrayTest {
         );
 
         assertEquals(resultadoEsperado, exc.getMessage());
+    }
+
+    @ParameterizedTest
+    @MethodSource("testParametrizadoC4C7")
+    @Tag("parametrizado")
+    @Tag("conExcepciones")
+    void testParametrizadoC8(int datos[], int elementoABorrar, String resultadoEsperado){
+        DataArray dataArray = new DataArray(datos);
+
+        DataException exc = assertThrows(
+                DataException.class,
+                ()->dataArray.delete(elementoABorrar));
+
+        assertEquals(resultadoEsperado, exc.getMessage());
+    }
+
+    private static Stream<Arguments> testParametrizadoC4C7(){
+        return Stream.of(
+                Arguments.of(new int[0], 8, "No hay elementos en la colección"),
+                Arguments.of(new int[]{1,3,5,7}, -5, "El valor a borrar debe ser > 0"),
+                Arguments.of(new int[0], 0, "Colección vacía. Y el valor a borrar debe ser > 0"),
+                Arguments.of(new int[]{1,3,5,7}, 8, "Elemento no encontrado")
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("testParametrizadoC1C3")
+    @Tag("parametrizado")
+    void testParametrizadoC9(int datos[], int elementoABorrar, int resultadoEsperadoColeccion[], int resultadoEsperadoNumElem){
+        DataArray dataArray = new DataArray(datos);
+        assertDoesNotThrow(
+                ()->dataArray.delete(elementoABorrar)
+        );
+        int resultadoColeccion[] = dataArray.getColeccion();
+        int resultadoNumElem = dataArray.size();
+
+        assertAll(
+                ()->assertArrayEquals(resultadoEsperadoColeccion, resultadoColeccion),
+                ()->assertEquals(resultadoEsperadoNumElem, resultadoNumElem)
+        );
+    }
+
+    private static Stream<Arguments> testParametrizadoC1C3(){
+        return Stream.of(
+                Arguments.of(new int[]{1,3,5,7}, 5, new int[]{1,3,7}, 3),
+                Arguments.of(new int[]{1,3,3,5,7}, 3, new int[]{1,3,5,7}, 4),
+                Arguments.of(new int[]{1,2,3,4,5,6,7,8,9,10}, 4, new int[]{1,2,3,5,6,7,8,9,10}, 9)
+        );
     }
 }
